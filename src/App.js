71f349d402b058db9158961797535b3287196b74
Marina <mara.berezin@gmail.com>
2020-09-23 00:55:46 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Logo from './Logo';
 import BestPockemonFetcher from './BestPocemon';
 import CaughtPockemon from './CaughtPockemon';
 import './App.css';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 
 
@@ -17,12 +18,25 @@ function App() {
   // }
 
   return (
-    <div className="App">
-      <Logo appName="Pokedex" />
-      <BestPockemonFetcher />
-      {/* <BestPockemon pockemon={NewPockemon} handleClick={logWhenTextClicked} /> */}
-      <CaughtPockemon />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div>
+          <Link to="/best-pockemon">Best Pockemon</Link>
+          <Link to="/caught-pockemon">Caught Pockemon</Link>
+        </div>
+        <Logo appName="Pokedex" />
+        <div>
+          <Route path="/best-pockemon" component={BestPockemonFetcher} />
+          <Route path="/caught-pockemon" component={CaughtPockemon} />
+        </div>
+
+
+        {/* <BestPockemon pockemon={NewPockemon} handleClick={logWhenTextClicked} /> */}
+        {/* <BestPockemonFetcher />
+        <CaughtPockemon /> */}
+      </div>
+    </BrowserRouter>
+
   );
 }
 
